91b0fd748965a85fb2666fc14dd29966b5f79b16
Javokhir1290 <ergashevazamera8@gmail.com>
2025-10-07 16:26:07 +0500

    Update, Add
    CustomUser FUnksiyasi yangiladi va migratsiya qilindi

--- common/models/auth.py
+++ common/models/auth.py
@@ -5,21 +5,38 @@ from django.contrib.auth.models import UserManager, AbstractBaseUser, Permission
 from django.db import models
 
 
-class CustomUserManager(UserManager):
-    def create_superuser(self, phone, password, **extra_fields):
-        user = self.model(
-            phone=phone,
-            **extra_fields
-        )
-        user.set_password(str(password))
-        user.save()
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError("Phone number is required")
+
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('user_type', 2)  # oddiy foydalanuvchi
+
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
         return user
 
 
-    def create_user(self, phone, password, **extra_fields):
-        extra_fields['is_staff'] = True
-        extra_fields['is_superuser'] = True
-        return self.create_superuser(self, phone=phone, password=password, **extra_fields)
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('user_type', 1)  # admin foydalanuvchi
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get('user_type') != 1:
+            raise ValueError("Superuser must have user_type=1.")
+
+        user = self.create_user(phone, password, **extra_fields)
+        return user
+
 
 
 
