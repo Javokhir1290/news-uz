0da2c1ebd0ae2546530a0d4f60c733adb7dba277
Javokhir1290 <ergashevazamera8@gmail.com>
2025-10-08 03:14:08 +0500

    Update

--- common/models/auth.py
+++ common/models/auth.py
@@ -24,7 +24,6 @@ class CustomUserManager(BaseUserManager):
 
 
     def create_superuser(self, phone, password=None, **extra_fields):
-        # bu yerda avtomatik qiymatlar beramiz
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('user_type', 1)  # majburan admin
